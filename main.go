f188f78db66c2be90705e7f6afaace40da2d598c
Mason Johnson <mason@invariant.dev>
2020-03-06 15:17:20 -0700

    Added general main page and concurrent file processing

--- main.go
+++ main.go
@@ -6,46 +6,73 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
+	"sync"
 
 	"github.com/masonj188/binanalysis/ganalyze/pinfo"
 )
 
+type LinkName struct {
+	Link string
+	Name string
+}
 type Pathlink struct {
-	LinkName []struct {
-		Link string
-		Name string
+	LinkNames []LinkName
+}
+
+func processFile(path string, outQueue chan<- LinkName) error {
+	f, err := os.Open(path)
+	if err != nil {
+		fmt.Println("Unable to open file: ", err)
+		return err
 	}
+	defer f.Close()
+	props := pinfo.NewProps(f, true)
+	outpath := strings.Join([]string{"report/", path, ".html"}, "")
+	err = props.ExportHTML(outpath)
+	if err != nil {
+		fmt.Println("Error exporting html for", props.Name)
+		return err
+	}
+	outQueue <- LinkName{strings.TrimPrefix(outpath, "report/"), filepath.Base(path)}
+	return nil
 }
 
 func main() {
 	links := Pathlink{}
+	fileQueue := make([]string, 0)
+	linkchan := make(chan LinkName)
+	var wg sync.WaitGroup
+
 	err := filepath.Walk(os.Args[1], func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
 		if info.IsDir() {
 			return nil
 		}
 		if ext := filepath.Ext(path); ext != ".exe" && ext != ".dll" {
 			return nil
 		}
-		f, err := os.Open(path)
-		if err != nil {
-			fmt.Println("Unable to open file:", err)
-		}
-		defer f.Close()
-		props := pinfo.NewProps(f, true)
-		outpath := strings.Join([]string{"report/", path, ".html"}, "")
-		err = props.ExportHTML(outpath)
-		if err != nil {
-			fmt.Println("Error exporting html for", props.Name)
-		}
-		links.LinkName = append(links.LinkName, struct {
-			Link string
-			Name string
-		}{strings.TrimPrefix(outpath, "report/"), filepath.Base(path)})
+		fileQueue = append(fileQueue, path)
 		return nil
 	})
+
 	if err != nil {
 		fmt.Println("Error walking filepath", err)
 	}
+
+	for _, file := range fileQueue {
+		wg.Add(1)
+		go processFile(file, linkchan)
+	}
+
+	go func() {
+		for result := range linkchan {
+			links.LinkNames = append(links.LinkNames, result)
+			wg.Done()
+		}
+	}()
+	wg.Wait()
 
 	t, err := template.ParseFiles("index.html.template")
 	if err != nil {
